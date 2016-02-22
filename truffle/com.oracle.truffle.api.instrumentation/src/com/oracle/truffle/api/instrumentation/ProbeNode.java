71f12a10e4cbaddf006107600f8fc6290b49434b
Stefan Marr <git@stefan-marr.de>
2016-02-22 15:24:47 +0100

    Add missing chain traversal, otherwise it loops endlessly
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- truffle/com.oracle.truffle.api.instrumentation/src/com/oracle/truffle/api/instrumentation/ProbeNode.java
+++ truffle/com.oracle.truffle.api.instrumentation/src/com/oracle/truffle/api/instrumentation/ProbeNode.java
@@ -154,6 +154,7 @@ public final class ProbeNode extends Node {
             if (currentChain.binding.getElement() == factory) {
                 return ((EventProviderChainNode) currentChain).eventNode;
             }
+            currentChain = currentChain.next;
         }
         return null;
     }
